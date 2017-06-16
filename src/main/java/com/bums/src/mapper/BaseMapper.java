ef4a2a8268db6812bbaccd315fbd7f69cbd12d86
kimjoin2@gmail.com <kimjoin2@gmail.com>
2017-06-16 16:41:28 +0900

    code for mybatis configuration and functions

--- src/main/java/com/bums/src/mapper/BaseMapper.java
+++ src/main/java/com/bums/src/mapper/BaseMapper.java
@@ -6,4 +6,13 @@ public interface BaseMapper {
 
 	// add user by UserDTO
 	public void insertUser(UserDTO user);
+	
+	// check for login
+	public String canLogin(UserDTO user);
+	
+	// delete user
+	public void deleteUser(UserDTO user);
+	
+	// change info of user
+	public void changePw(UserDTO user);
 }
